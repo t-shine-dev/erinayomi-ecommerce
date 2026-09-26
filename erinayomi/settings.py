af470cb536fb1aa6dff16a48538d096bed0634d5
Temitope Olawuyi <temitopeolawuyi001@gmail.com>
2026-09-26 09:18:58 +0100

    Remove conflicting cloudinary-storage package and use native cloudinary configuration

--- erinayomi/settings.py
+++ erinayomi/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "wishlist",
     "store_settings",
     "payments",
-    # Cloudinary storage (this includes the core backend cleanly)
-    "cloudinary_storage",
+    # Core Cloudinary app
+    "cloudinary",
 ]
 
 if USE_CLOUDINARY:
@@ -124,11 +124,15 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
-CLOUDINARY_STORAGE = {
-    "CLOUDINARY_URL": config("CLOUDINARY_URL", default="")
-}
+# Configure native Cloudinary storage safely
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+cloudinary.config(
+    cloudinary_url=config("CLOUDINARY_URL", default="")
+)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
