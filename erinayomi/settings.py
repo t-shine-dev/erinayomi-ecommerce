9ba166bc4d3902219200a297a2c24d06343bee5c
Temitope Olawuyi <temitopeolawuyi001@gmail.com>
2026-09-26 10:08:28 +0100

    Remove conflicting STATICFILES_STORAGE setting

--- erinayomi/settings.py
+++ erinayomi/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"  # <-- REMOVE THIS LINE
 
 MEDIA_URL = "/media/"
 
